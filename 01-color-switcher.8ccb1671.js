ee244413f3fbebe3c0a5284135c826589c62ef92
stanislav-kintoriak <114350380+stanislav-kintoriak@users.noreply.github.com>
2023-03-26 09:02:08 +0000

    Deploying to gh-pages from @ stanislav-kintoriak/goit-js-hw-10@d8a170b6ebbb01e65df2f46753295b97502a6456 🚀

--- 01-color-switcher.8ccb1671.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.querySelector("body");let a=null;function n(){t.disabled=!1,e.disabled=!0,d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}e.addEventListener("click",(function(){a=setInterval(n,1e3)})),t.addEventListener("click",(function(){clearInterval(a),t.disabled=!0,e.disabled=!1})),t.disabled=!0;
-//# sourceMappingURL=01-color-switcher.8ccb1671.js.map
